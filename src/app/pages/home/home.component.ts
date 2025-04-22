e5a9fa100b85664b221ccc3ed54a4eb9c235842f
Camille <camille.cabrol1@etu.univ-grenoble-alpes.fr>
2025-04-22 20:05:23 +0200

    Mise en place de la page d'accueil

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ButtonModule} from 'primeng/button';
+import {CommonModule} from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule, ButtonModule, RouterModule],
+  standalone: true,
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  telechargerCV() {
+
+  }
 
 }
+
