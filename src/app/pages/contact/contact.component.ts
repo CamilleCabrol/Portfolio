d457f7443bf370442a4e0f080af310bb5a618364
Camille <camille.cabrol1@etu.univ-grenoble-alpes.fr>
2025-04-22 23:41:39 +0200

    Page contact en forme

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {CardModule} from 'primeng/card';
+import {DividerModule} from 'primeng/divider';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [CommonModule, CardModule, DividerModule],
+  standalone: true,
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  openLinkedIn() {
+    window.open('https://www.linkedin.com/in/camille-cabrol-7b6648169/', '_blank');
+  }
 
+  sendMail() {
+    window.location.href = 'mailto:camille.cabrol@free.fr';
+  }
 }
