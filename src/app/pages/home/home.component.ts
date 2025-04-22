0cc1abcbe6db426c317cf1754f52b22cd429e05c
Camille <camille.cabrol1@etu.univ-grenoble-alpes.fr>
2025-04-22 21:49:09 +0200

    Page d'accueil ok

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 import {ButtonModule} from 'primeng/button';
 import {CommonModule} from '@angular/common';
-import { RouterModule } from '@angular/router';
+import {RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -11,9 +11,13 @@ import { RouterModule } from '@angular/router';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  telechargerCV() {
+  telechargerCV(): void {
+    const fileUrl = 'assets/CV_CABROL_Camille_Developpeuse.pdf';
 
+    const a = document.createElement('a');
+    a.href = fileUrl;
+    a.download = 'CV_CABROL_Camille_Developpeuse.pdf';
+    a.click();
   }
-
 }
 
